44547efca72989038fe83ca6fdfd947f857bcd74
Jordan <jordan.wallace.williams@gmail.com>
2020-09-23 17:04:23 -0600

    Add fetch_pay_stubs and fetch_employments methods. Each method corresponds to the equivalent Zenefits API endpoint

--- tap_zenefits/zenefits.py
+++ tap_zenefits/zenefits.py
@@ -6,23 +6,71 @@ import os
 
 load_dotenv()
 
+company = '76795'
+
 API_KEY = os.getenv("API_KEY")
 
 headers = {
     'Authorization': API_KEY
 }
 
-payload = {}
-
-async def fetch(client):
-  async with client.get("https://api.zenefits.com/core/companies/76795/people", headers=headers, data=payload) as resp:
-    return await resp.json()
-
-
 async def main():
   async with aiohttp.ClientSession() as client:
-    json_response = await fetch(client)
-    print(json_response)
+    people_response = await fetch_people(client)
+    # The pay_stubs endpoint is not able to be authorized
+    # pay_stubs_response = await fetch_pay_stubs(client)
+    employments_response = await fetch_employments(client)
+
+    return people_response, employments_response
+
+
+async def fetch_people(client):
+  async with client.get(f"https://api.zenefits.com/core/companies/{company}/people", headers=headers) as resp:
+    response = await resp.json()
+    people = people_dict(response)
+    return people
+
+
+async def fetch_pay_stubs(client):
+  async with client.get("https://api.zenefits.com/payroll/payrun_pay_stubs", headers=headers) as resp:
+    response = await resp.json()
+    return json.dumps(response)
+
+
+async def fetch_employments(client):
+  async with client.get("https://api.zenefits.com/core/employments", headers=headers) as resp:
+    response = await resp.json()
+    employments = employments_dict(response)
+    return employments
+
+
+def people_dict(response):
+  employees = {}
+
+  for person in response['data']['data']:
+    employees[person['id']] = {
+      'first_name': person['first_name'],
+      'last_name': person['last_name'],
+      'employee_number': person['employee_number'],
+      'manager': person['manager']['url'],
+      'department': person['department']['url'],
+      'title': person['title'],
+      'work_email': person['work_email'],
+      'work_phone': person['work_phone'],
+      'photo_url': person['photo_url']
+    }
+
+  return json.dumps(employees)
+
+
+def employments_dict(response):
+  employments = {}
+
+  for person in response['data']['data']:
+    # This returns the employment id, but it could return the people id
+    employments[person['id']] = person
+  
+  return json.dumps(employments)
 
 
 loop = asyncio.get_event_loop()
