b35c13ca1521127ef35535053328cc88907f380e
Jordan <jordan.wallace.williams@gmail.com>
2020-09-23 17:06:30 -0600

    Update test file to import the zenefits.py module. Tests need to be written using VCR in order to test responses

--- tests/test_tap_zenefits.py
+++ tests/test_tap_zenefits.py
@@ -1,5 +1,7 @@
-from tap_zenefits import __version__
+from tap_zenefits.zenefits import *
 
+def add_2(num):
+    return num + 2
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_add_2():
+    add_2(3) == 5
