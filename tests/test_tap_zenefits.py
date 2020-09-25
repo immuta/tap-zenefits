488edf9f3069c15bde50b601a7fd7fcd494d32e3
Jordan <jordan.wallace.williams@gmail.com>
2020-09-24 21:23:32 -0600

    Add VCR, pytest_asyncio, and pytest_vcr to imports. Add tests for fetch_people and fetch_employments

--- tests/test_tap_zenefits.py
+++ tests/test_tap_zenefits.py
@@ -1,7 +1,38 @@
+import vcr
+import pytest
+import pytest_asyncio
+import pytest_vcr
 from tap_zenefits.zenefits import *
 
-def add_2(num):
-    return num + 2
+# @pytest.fixture
+# async def service(endpoint):
+#     async with aiohttp.ClientSession() as client:
+#         return await endpoint(client)
 
-def test_add_2():
-    add_2(3) == 5
+
+@pytest.mark.vcr()
+@pytest.mark.asyncio
+async def test_fetch_people():
+    async with aiohttp.ClientSession() as client:
+        response = await fetch_people(client)
+    
+    assert response['status'] == 200
+    assert 'first_name' in response['data']['data'][0]
+    assert 'last_name' in response['data']['data'][0]
+    assert 'employee_number' in response['data']['data'][0]
+    assert 'manager' in response['data']['data'][0]
+    assert 'department' in response['data']['data'][0]
+    assert 'title' in response['data']['data'][0]
+    assert 'work_email' in response['data']['data'][0]
+    assert 'work_phone' in response['data']['data'][0]
+    assert 'photo_url' in response['data']['data'][0]
+
+
+@pytest.mark.vcr()
+@pytest.mark.asyncio
+async def test_fetch_employments():
+    async with aiohttp.ClientSession() as client:
+        response = await fetch_employments(client)
+    
+    assert response['status'] == 200
+
