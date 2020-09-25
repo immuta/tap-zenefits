d3514a281e24b97c09696ce073d87da874065542
Jordan <jordan.wallace.williams@gmail.com>
2020-09-25 11:41:53 -0600

    All helper methods for modeling data have been moved to the module helpers_dandelion.py

--- tap_zenefits/zenefits.py
+++ tap_zenefits/zenefits.py
@@ -4,6 +4,7 @@ import json
 from dotenv import load_dotenv
 import os
 from collections import defaultdict
+from tap_zenefits.helpers_dandelion import *
 
 load_dotenv()
 
@@ -60,58 +61,6 @@ async def fetch_time_durations(client):
         response = await resp.json()
         time_durations = time_durations_dict(response)
         return time_durations
-
-
-def people_dict(response):
-    employees = {}
-
-    for person in response['data']['data']:
-        employees[person['id']] = {
-            'first_name': person['first_name'],
-            'last_name': person['last_name'],
-            'employee_number': person['employee_number'],
-            'manager': person['manager']['url'],
-            'department': person['department']['url'],
-            'title': person['title'],
-            'work_email': person['work_email'],
-            'work_phone': person['work_phone'],
-            'photo_url': person['photo_url']
-        }
-
-    return json.dumps(employees)
-
-
-def employments_dict(response):
-    employments = {}
-
-    for person in response['data']['data']:
-        # This returns the employment id, but it could return the people id
-        employments[person['id']] = person
-
-    return json.dumps(employments)
-
-
-def departments_dict(response):
-    departments = {}
-
-    for department in response['data']['data']:
-        # This returns the department id
-        departments[department['id']] = department
-
-    return json.dumps(departments)
-
-
-def time_durations_dict(response):
-    time_durations = {}
-    time_durations = defaultdict(lambda: [], time_durations)
-
-    for duration in response['data']['data']:
-        # This returns the person id, but it could return the duration id.
-        # The person id has a value of a list that contains all durations
-        person = duration['person']['url'].split("people/")
-        time_durations[person[1]].append(duration)
-
-    return json.dumps(time_durations)
 
 
 loop = asyncio.get_event_loop()
