4da9970cdfe49c9240ff736f00dd1a370e1a9b35
Michael Cooper <macoop2363@gmail.com>
2020-10-19 14:36:39 -0600

    Add setup file in order for the singer to be turned into a package

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-zenefits",
+    version="0.1.0",
+    description="Singer.io tap for extracting data from the Zenefits API",
+    author="Josh Temple",
+    url="https://www.zenefits.com/",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_zenefits"],
+    install_requires=[
+        "singer-python==5.9.0",
+        "requests",
+        "aiohttp==3.6.2"
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-zenefits=tap_zenefits:main
+    """,
+    packages=["tap_zenefits"],
+    package_data = {
+        "schemas": ["tap_zenefits/schemas/*.json"]
+    },
+    include_package_data=True,
+)
